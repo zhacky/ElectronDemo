9812fc03c51b0320b1989e65f41f1f85492d1c31
Zhack Ariya <zhackariya@gmail.com>
2018-11-14 02:17:56 +0800

    calendar init

--- scripts/main-calendar.js
+++ scripts/main-calendar.js
@@ -1,8 +1,93 @@
-// jQuery(function() {
-// console.log('hey calendar');
-//                           // page is now ready, initialize the calendar...
 
-//                           $('#calendar').fullCalendar({
-//                             // put your options and callbacks here
-//                           });
-// });
+
+$(function(){
+
+
+    var todayDate = moment().startOf('day');
+    var YM = todayDate.format('YYYY-MM');
+    var YESTERDAY = todayDate.clone().subtract(1, 'day').format('YYYY-MM-DD');
+    var TODAY = todayDate.format('YYYY-MM-DD');
+    var TOMORROW = todayDate.clone().add(1, 'day').format('YYYY-MM-DD');
+//demo
+    var events = [
+        {
+            title: 'All Day Event',
+            start: YM + '-01'
+        },
+        {
+            title: 'Long Event',
+            start: YM + '-07',
+            end: YM + '-10'
+        },
+        {
+            id: 999,
+            title: 'Repeating Event',
+            start: YM + '-09T16:00:00'
+        },
+        {
+            id: 999,
+            title: 'Repeating Event',
+            start: YM + '-16T16:00:00'
+        },
+        {
+            title: 'Conference',
+            start: YESTERDAY,
+            end: TOMORROW
+        },
+        {
+            title: 'Meeting',
+            start: TODAY + 'T10:30:00',
+            end: TODAY + 'T12:30:00'
+        },
+        {
+            title: 'Lunch',
+            start: TODAY + 'T12:00:00'
+        },
+        {
+            title: 'Meeting',
+            start: TODAY + 'T14:30:00'
+        },
+        {
+            title: 'Happy Hour',
+            start: TODAY + 'T17:30:00'
+        },
+        {
+            title: 'Dinner',
+            start: TODAY + 'T20:00:00'
+        },
+        {
+            title: 'Birthday Party',
+            start: TOMORROW + 'T07:00:00'
+        },
+        {
+            title: 'Click for Google',
+            url: 'http://google.com/',
+            start: YM + '-28'
+        }
+    ];
+//end demo
+$('#calendar').fullCalendar({
+
+
+    header: {
+        left: 'prev,next today',
+        center: 'title',
+        // right: 'month,agendaWeek,agendaDay,listWeek'
+        right: 'month,agendaDay'
+    },
+    // weekends: false,
+    editable: true,
+    eventLimit: true, // allow "more" link when too many events
+    navLinks: true,
+    events: []
+}); // end calendar
+
+
+});
+
+function countProfiles(){
+    for (let i = 0; i < profiles.length; i++) {
+        const profile = profiles[i];
+        console.log('profile --' + profile + ' - ' + i);
+    }
+}
